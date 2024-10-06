c9b357db1aebe72e360b0560d2b534f3f215f7e4
Buzhiliao <29416664+fortwind@users.noreply.github.com>
2024-10-06 23:29:30 +0800

    Update mail-worker.sh

--- worker/mail-worker.sh
+++ worker/mail-worker.sh
@@ -15,19 +15,19 @@ main = "src/worker.ts"
 compatibility_date = "2024-09-23"
 compatibility_flags = [ "nodejs_compat" ]
 routes = [
-    { pattern = "{{EMAIL_API_DOMAIN}}", custom_domain = true },
+    { pattern = "${EMAIL_API_DOMAIN}", custom_domain = true },
 ]
 
 [vars]
 PREFIX = ""
-PASSWORDS = ["{{PASSWORDS}}"]
-ADMIN_PASSWORDS = ["{{ADMIN_PASSWORDS}}"]
-DOMAINS = ["{{EMAIL_DOMAIN}}"] # all domain names
-ADMIN_USER_ROLE = "{{ADMIN_USER_ROLE}}"
+PASSWORDS = ["${PASSWORDS}"]
+ADMIN_PASSWORDS = ["${ADMIN_PASSWORDS}"]
+DOMAINS = ["${EMAIL_DOMAIN}"] # all domain names
+ADMIN_USER_ROLE = "${ADMIN_USER_ROLE}"
 USER_ROLES = [
-   { domains = ["{{EMAIL_DOMAIN}}"], role = "{{ADMIN_USER_ROLE}}", prefix = "" },
+   { domains = ["${EMAIL_DOMAIN}"], role = "${ADMIN_USER_ROLE}", prefix = "" },
 ]
-JWT_SECRET = "{{JWT_SECRET}}"
+JWT_SECRET = "${JWT_SECRET}"
 BLACK_LIST = ""
 ENABLE_USER_CREATE_EMAIL = true
 ENABLE_USER_DELETE_EMAIL = true
@@ -35,6 +35,6 @@ ENABLE_AUTO_REPLY = false
 
 [[d1_databases]]
 binding = "DB"
-database_name = "{{DB_NAME}}"
-database_id = "{{DB_ID}}"
+database_name = "${DB_NAME}"
+database_id = "${DB_ID}"
 EOF
