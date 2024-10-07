13105822c5475f9f3c5d7f9f47d6dc9e0268f79d
Buzhiliao <29416664+fortwind@users.noreply.github.com>
2024-10-07 22:06:03 +0800

    1

--- worker/mail-worker.sh
+++ worker/mail-worker.sh
@@ -5,9 +5,10 @@
 # PASSWORDS
 # ADMIN_PASSWORDS
 # ADMIN_USER_ROLE
-# JWT_SECRET
 # DB_NAME
 # DB_ID
+# JWT_SECRET
+# RESEND_TOKEN
 
 cat > wrangler.toml <<EOF
 name = "cloudflare-temp-email"
@@ -27,7 +28,6 @@ ADMIN_USER_ROLE = "${ADMIN_USER_ROLE}"
 USER_ROLES = [
    { domains = ["${EMAIL_DOMAIN}"], role = "${ADMIN_USER_ROLE}", prefix = "" },
 ]
-JWT_SECRET = "${JWT_SECRET}"
 BLACK_LIST = ""
 ENABLE_USER_CREATE_EMAIL = true
 ENABLE_USER_DELETE_EMAIL = true
@@ -40,4 +40,9 @@ database_id = "${DB_ID}"
 
 [observability]
 enabled = true
+EOF
+
+cat > .dev.vars <<EOF
+JWT_SECRET="${JWT_SECRET}"
+RESEND_TOKEN="${RESEND_TOKEN}"
 EOF
