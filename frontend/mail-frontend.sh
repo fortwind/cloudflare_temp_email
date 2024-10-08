988b4107c37c779569c9c43bf673ad90b40d0682
Buzhiliao <29416664+fortwind@users.noreply.github.com>
2024-10-08 21:06:14 +0800

    support 204

--- frontend/mail-frontend.sh
+++ frontend/mail-frontend.sh
@@ -1,7 +1,35 @@
 # #!/bin/sh
 
+uid=$(cat /dev/urandom | tr -dc 'a-z0-9' | head -c 18)
+
+echo "uid: ${uid}"
+
 pnpm build:pages
 cp -rf ../pages/functions .
+
+sed -i 's@export async function onRequest@async function onRequest1@' ./functions/_middleware.js
+
+cat >> ./functions/_middleware.js <<EOF
+
+export async function onRequest(context) {
+    const reqPath = new URL(context.request.url).pathname
+    if (reqPath === '/${uid}' && context.request.method === 'GET') {
+        return new Response('', {
+            status: 301,
+            headers: {
+                'Set-Cookie': 'uid=${uid}; Max-Age=2592000; Secure; HttpOnly'
+            }
+        })
+    }
+
+    const cookie = context.request.headers.get('cookie')
+    if (!cookie || !cookie.includes('uid=${uid};')) {
+        return new Response(null, { status: 204 })
+    }
+
+    return onRequest1(context)
+}
+EOF
 
 # cat > wrangler.toml <<EOF
 # name = "temp-email-pages"
